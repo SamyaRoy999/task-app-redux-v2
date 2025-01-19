91178c320ea3754db57d861db24233ef58d0aafb
samya-roy <samyaroy999@gmail.com>
2025-01-19 23:57:23 +0600

    intregreat onAuthStateChange

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,9 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
+  const { email, name } = useSelector((state) => state.userSlice);
+  console.log(email, name);
+
   return (
     <div>
       <h1>Profile </h1>
